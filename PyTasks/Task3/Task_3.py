cc8578f83a0b6998c6321bc04c6f3d7d82ec53cb
Dima <dimitriy648@yandex.ru>
2022-02-23 23:37:13 +0300

    added tasks1-5

---
+++ PyTasks/Task3/Task_3.py
@@ -0,0 +1,9 @@
+def main(m, a, x):
+    ans = 0
+    for i in range(1, a + 1):
+        for c in range(1, m + 1):
+            ans += 63 * x ** 2 + c + 73 * (i / 39 + 85 + 93 * i ** 2) ** 7
+    return ans
+
+
+main(4, 8, -0.95)
