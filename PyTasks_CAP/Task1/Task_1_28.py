2d60730ca7ec678aeb8e99a65ae1c34b74e71af2
Dima <dimitriy648@yandex.ru>
2022-06-07 16:25:57 +0300

    added task_1_28

--- PyTasks_CAP/Task1/Task_1_28.py
+++ PyTasks_CAP/Task1/Task_1_28.py
@@ -1,12 +1,8 @@
 # Вариант 28
 
-
-from math import sqrt
-
-
 def main(x, y):
     ans = ((70 * x) ** 3 - (y - y ** 2 - 35)) / (23 * ((x ** 3 - y ** 2 - 8 * y) ** 4))
-    ans += sqrt(abs(y / 85 - (x ** 2) / 22 - y ** 3) ** 5)
+    ans += (abs(y / 85 - (x ** 2) / 22 - y ** 3) ** 5) ** 0.5
     return ans
 
 
@@ -21,14 +17,10 @@ if __name__ == "__main__":
     res_3 = main(0.36, -0.81)
     res_4 = main(-0.54, -0.6)
     res_5 = main(-0.42, -0.37)
-    res_1 = "{:.2e}".format(res_1)
-    res_2 = "{:.2e}".format(res_2)
-    res_3 = "{:.2e}".format(res_3)
-    res_4 = "{:.2e}".format(res_4)
-    res_5 = "{:.2e}".format(res_5)
-    print('Res 1:', res_1)
-    print('Res 2:', res_2)
-    print('Res 3:', res_3)
-    print('Res 4:', res_4)
-    print('Res 5:', res_5)
+    print('Res 1:', "{:.2e}".format(res_1))
+    print('Res 2:', "{:.2e}".format(res_2))
+    print('Res 3:', "{:.2e}".format(res_3))
+    print('Res 4:', "{:.2e}".format(res_4))
+    print('Res 5:', "{:.2e}".format(res_5))
+
     print("\n", "-" * 20, "Done!", "-" * 20)
